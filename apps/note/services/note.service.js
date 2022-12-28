8c0e76ad12a44682d0141b05302f53dc6ad67383
eli-shallev <eli.shallev50@gmail.com>
2022-12-28 11:51:20 +0200

    basic crud for note

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,33 @@
-console.log('Hi')
+import { asyncStorageService } from "../../../services/async-storage.service.js"
+import {storageService} from '../../../services/storage.service.js'
+import { utilService } from "../../../services/util.service.js"
+
+const NOTE_KRY = 'noteDB'
+
+function query() {
+    return asyncStorageService.query(NOTE_KRY)
+}
+
+function getNote(noteId) {
+    return asyncStorageService.get(NOTE_KRY, noteId)
+}
+
+function removeNote(noteId) {
+    return asyncStorageService.remove(NOTE_KRY, noteId)
+}
+
+function saveNote(note) {
+    if (note.id) {
+        return asyncStorageService.put(NOTE_KRY, note)
+    } else {
+        return asyncStorageService.post(NOTE_KRY, note)
+    }
+}
+
+function getEmptyNote(type='',isPinned = false) {
+    return { type,isPinned, info:{},style:{} }
+  }
+  
+  function getDefaultFilter() {
+    return { txt: '', type: '' }
+  }
