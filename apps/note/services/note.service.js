0ec5f5c5336df5ab4b1022f337a41f493d86ade2
eli-shallev <eli.shallev50@gmail.com>
2022-12-28 12:29:13 +0200

    note service updated

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,33 +1,59 @@
 import { asyncStorageService } from "../../../services/async-storage.service.js"
-import {storageService} from '../../../services/storage.service.js'
+import { storageService } from '../../../services/storage.service.js'
 import { utilService } from "../../../services/util.service.js"
 
-const NOTE_KRY = 'noteDB'
+const NOTE_KEY = 'noteDB'
+
+export const noteService = {
+    query,
+    getNote,
+    removeNote,
+    saveNote,
+    getEmptyNote,
+    getDefaultFilter,
+}
 
 function query() {
-    return asyncStorageService.query(NOTE_KRY)
+    return asyncStorageService.query(NOTE_KEY)
 }
 
 function getNote(noteId) {
-    return asyncStorageService.get(NOTE_KRY, noteId)
+    return asyncStorageService.get(NOTE_KEY, noteId)
 }
 
 function removeNote(noteId) {
-    return asyncStorageService.remove(NOTE_KRY, noteId)
+    return asyncStorageService.remove(NOTE_KEY, noteId)
 }
 
 function saveNote(note) {
     if (note.id) {
-        return asyncStorageService.put(NOTE_KRY, note)
+        return asyncStorageService.put(NOTE_KEY, note)
     } else {
-        return asyncStorageService.post(NOTE_KRY, note)
+        return asyncStorageService.post(NOTE_KEY, note)
     }
 }
 
-function getEmptyNote(type='',isPinned = false) {
-    return { type,isPinned, info:{},style:{} }
-  }
-  
-  function getDefaultFilter() {
+function getEmptyNote(type = '', isPinned = false) {
+    return { type, isPinned, info: {}, style: {} }
+}
+
+function getDefaultFilter() {
     return { txt: '', type: '' }
-  }
+}
+
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTE_KEY)
+    if (!notes || !notes.length) {
+        notes = []
+        notes.unshift(_createNote('note-txt', true))
+        notes.unshift(_createNote('note-txt', false))
+        notes.unshift(_createNote('note-txt', false))
+        utilService.saveToStorage(NOTE_KEY, notes)
+    }
+}
+
+function _createNote(type, isPinned) {
+    const note = getEmptyNote(type, isPinned)
+    note.id = utilService.makeId()
+    note.info.txt = utilService.makeLorem(10)
+}
