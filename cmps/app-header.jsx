aa3924c27f873e0d36e7fb59058057a09329dd27
Irene Duchovny <ibalakin@gmail.com>
2022-12-30 16:48:33 +0200

    header css initial update

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -19,19 +19,23 @@ export function AppHeader() {
     nav(to);
   }
 
-    return <header className="app-header">
+    return <header className="app-header flex space-between ">
         
         <Link to="/">
         { isMail ? <div><img src="assets/img/mail-logo.png"/></div> : (isNote ?<div><img src="assets/img/notes-logo.png"/></div> : <img src="assets/img/appsus-logo-1.png" alt="" />)}
 
             {/* <h3>Apsus!</h3> */}
         </Link>
-       
+       <div className="search-bar-header">
+        <input className="searchbar-input" type="text" placeholder="Search" />
+       </div>
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <a onClick={() => goMail('mail')}>Mail</a>
-            <a onClick={() =>goNote('note')}>Note</a>
+          <div className="main-nav-btns flex ">
+          <button className="go-home-btn">  <NavLink to="/">Home</NavLink></button>
+          <button className="go-about-btn"><NavLink to="/about">About</NavLink></button>
+          <button className="go-home-btn"> <a onClick={() => goMail('mail')}>E-mail</a></button>
+          <button className="go-note-btn"><a onClick={() =>goNote('note')}>Note</a></button>
+          </div>
         </nav>
     </header>
 }
