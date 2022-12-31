ac1d75cd805afb048ac75ba91febaf3d3edf7961
Irene Duchovny <ibalakin@gmail.com>
2022-12-31 21:06:16 +0200

    last bug-final submission

--- views/home.jsx
+++ views/home.jsx
@@ -1,55 +1,50 @@
 
-const { Link } = ReactRouterDOM
+const { Link, useNavigate } = ReactRouterDOM
 
-
-
-export function Home( { setIsBook, setIsMail, setIsNote}) {
+export function Home({ setIsBook, setIsMail, setIsNote }) {
+    const nav = useNavigate()
     function goMail(to) {
-
         setIsMail(true)
         setIsNote(false)
         setIsBook(false)
         closeMenu()
         nav(to);
-      }
-    
-      function goNote(to) {
-    
+    }
+
+    function goNote(to) {
         setIsMail(false)
         setIsNote(true)
         setIsBook(false)
         closeMenu()
         nav(to);
-      }
-    
-      function goBook(to) {
-    
+    }
+
+    function goBook(to) {
         setIsMail(false)
         setIsNote(false)
         setIsBook(true)
         closeMenu()
         nav(to);
-      }
-    
-      function closeMenu() {
+    }
+
+    function closeMenu() {
         document.body.classList.toggle('menu-open')
-      }
+    }
 
     return <section className="home">
         <div className="main-page-container flex space-around">
             <Link to="/mail">
-                <div className="mail-container" onClick={() => {goMail('/mail')}}>
-
+                <div className="mail-container" onClick={() => { goMail('/mail') }}>
                     <img src="assets/img/mail-logo.png" alt="" />
                     <h2>E-mails </h2>
                 </div></Link>
             <Link to="/note">
-                <div className="notes-container" onClick={() => {goNote('/note')}}>
+                <div className="notes-container" onClick={() => { goNote('/note') }}>
                     <img src="assets/img/notes-logo.png" alt="" />
                     <h2>Notes </h2>
                 </div></Link>
             <Link to="/book">
-                <div className="books-container" onClick={() => {goBook('/book')}}>
+                <div className="books-container" onClick={() => { goBook('/book') }}>
                     <img src="assets/img/books-logo.png" alt="" />
                     <h2> Books</h2>
                 </div></Link>
