697f9adecfbdb107ff5a36e89f0d6e383dc64dcc
Irene Duchovny <ibalakin@gmail.com>
2022-12-31 20:04:00 +0200

    css and links update

--- views/home.jsx
+++ views/home.jsx
@@ -1,23 +1,52 @@
 
 const { Link } = ReactRouterDOM
 
-export function Home() {
+
+
+export function Home( { setIsBook, setIsMail, setIsNote}) {
+    function goMail(to) {
+
+        setIsMail(true)
+        setIsNote(false)
+        setIsBook(false)
+        closeMenu()
+        nav(to);
+      }
+    
+      function goNote(to) {
+    
+        setIsMail(false)
+        setIsNote(true)
+        setIsBook(false)
+        closeMenu()
+        nav(to);
+      }
+    
+      function goBook(to) {
+    
+        setIsMail(false)
+        setIsNote(false)
+        setIsBook(true)
+        closeMenu()
+        nav(to);
+      }
+    
 
     return <section className="home">
         <div className="main-page-container flex space-around">
             <Link to="/mail">
-                <div className="mail-container">
+                <div className="mail-container" onClick={() => {goMail('/mail')}}>
 
                     <img src="assets/img/mail-logo.png" alt="" />
                     <h2>E-mails </h2>
                 </div></Link>
             <Link to="/note">
-                <div className="notes-container">
+                <div className="notes-container" onClick={() => {goNote('/note')}}>
                     <img src="assets/img/notes-logo.png" alt="" />
                     <h2>Notes </h2>
                 </div></Link>
             <Link to="/book">
-                <div className="books-container">
+                <div className="books-container" onClick={() => {goBook('/book')}}>
                     <img src="assets/img/books-logo.png" alt="" />
                     <h2> Books</h2>
                 </div></Link>
