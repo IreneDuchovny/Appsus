8df9930b46fcd00b98477ce3e957c8d4534ad0e1
Irene Duchovny <ibalakin@gmail.com>
2022-12-31 15:26:09 +0200

    mail integration to notes

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,11 +1,11 @@
 
-const { Link } = ReactRouterDOM
-const { useEffect, useState, useRef,Fragment } = React
+const { Link, useNavigate } = ReactRouterDOM
+const { useEffect, useState, useRef, Fragment } = React
 import { Longtxt } from '/long-txt.jsx'
 
-export function MailPreview({ mail, onDeleteMail, onSaveMail ,getUnreadCount}) {
+export function MailPreview({ mail, onDeleteMail, onSaveMail, getUnreadCount }) {
     const [isStarred, setIsStarred] = useState(mail.isStarred)
-
+    const navigate = useNavigate()
     const [isRead, setIsRead] = useState(mail.isRead)
     // const [isSent, setIsSent] = useState(mail.isSent)
     // const [isDraft, setIsDraft] = useState(mail.isDraft)
@@ -38,9 +38,13 @@ export function MailPreview({ mail, onDeleteMail, onSaveMail ,getUnreadCount}) {
         };
 
         const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-
         return formattedDate
+    }
 
+    function handleNoteSend() {
+        const queryStringParams = `?title=${mail.subject}&txt=${mail.body}`
+        const newUrl = '../' + '/note' + queryStringParams
+        navigate(newUrl)
     }
 
     return <div className="expend-prev flex column" >
@@ -48,7 +52,7 @@ export function MailPreview({ mail, onDeleteMail, onSaveMail ,getUnreadCount}) {
         <div className={isRead ? "main-mail-preview flex row " : "main-mail-preview flex row space-between unread"} onClick={() => {
             onClickMail()
         }}>
-           
+
             {/* <input className="starred-checkbox fa-star-o" title="Starred email"  defaultChecked={isStarred ? 'cheched' : ''} /> */}
             <button className={isStarred ? "fa fa-star starred-checkbox" : "fa fa-star-o starred-checkbox"} title="Starred email" onClick={() => {
                 onStarMail()
@@ -65,6 +69,7 @@ export function MailPreview({ mail, onDeleteMail, onSaveMail ,getUnreadCount}) {
         {/* //expended section */}
         <div className="expended-mail" hidden={!isExpanded}>
             <div className="expended-mail-btn flex row flex-end">
+                <button className="fa-solid fa-paper-plane note-btn" onClick={handleNoteSend}></button>
                 <button className={isStarred ? "fa fa-star star-btn" : "fa fa-star-o star-btn"} title="Starred email" onClick={() => {
                     onStarMail()
 
@@ -73,22 +78,22 @@ export function MailPreview({ mail, onDeleteMail, onSaveMail ,getUnreadCount}) {
                 <Link to={`/mail/${mail.id}`}><button className="fa fa-expand expant-btn" title="Expand"></button> </Link>
             </div>
             <div className="expended-mail-content flex row">
-            <div className="main-pereview-expended ">
-            <img  className= "list-sender-image" src="assets/img/user.png" /></div>
-            <div className="expended-mail-content">
-            <h3 className="email-subject">{mail.subject} </h3>
-            <p className="from-email">from: {mail.name} {`<${mail.to}>`}</p>
-            <p className="to-email">To: me</p>
-            <p className="email-body">{mail.body}</p></div>
+                <div className="main-pereview-expended ">
+                    <img className="list-sender-image" src="assets/img/user.png" /></div>
+                <div className="expended-mail-content">
+                    <h3 className="email-subject">{mail.subject} </h3>
+                    <p className="from-email">from: {mail.name} {`<${mail.to}>`}</p>
+                    <p className="to-email">To: me</p>
+                    <p className="email-body">{mail.body}</p></div>
             </div>
         </div>
     </div>
-    
 
-//   <div className="list-preview-header flex column ">
-//            <img src="assets/img/mail-inbox.png" />
-//            <div className="primary">Primary</div>
-//        </div>
+
+    //   <div className="list-preview-header flex column ">
+    //            <img src="assets/img/mail-inbox.png" />
+    //            <div className="primary">Primary</div>
+    //        </div>
 
 
 
